ff213cbd79207a6bea40c0ee086c779401b2d242
Accordis-Tobi <125647887+Accordis-Tobi@users.noreply.github.com>
2023-05-26 15:39:00 +0200

    a tag

--- src/components/about/AboutClientSingle.vue
+++ src/components/about/AboutClientSingle.vue
@@ -6,12 +6,10 @@ export default {
 
 <template>
 	<div>
-		<a :href="client.link">
+		<a :href="client.link" target="_blank">
 			<img
 				:src="client.img"
 				:alt="client.title"
-				
-				target="_blank"
 				class="w-64 h-40 object-contain py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer dark:bg-secondary-light"
 			/>
 		</a>
