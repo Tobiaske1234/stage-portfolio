6708a637211356d9586b8d43eb2af93938d93355
Tobiaske1234 <tobi.de.fijter@icloud.com>
2023-06-19 16:20:24 +0200

    microsoft clarity attempt 2

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,6 @@ export default {
 </script>
 
 <template>
-	<script type="text/javascript">
-		(function(c,l,a,r,i,t,y){
-			c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-			t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-		})(window, document, "clarity", "script", "hmcxslcjxv");
-	</script>
 	<div :class="appTheme" class="pt-0.5">
 		<!-- App header -->
 		<AppHeader />
--- src/main.js
+++ src/main.js
@@ -13,6 +13,12 @@ Hotjar.init(siteId, hotjarVersion);
 
 inject();
 
+(function(c,l,a,r,i,t,y){
+	c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+	t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+	y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "hmcxslcjxv");
+
 const feather = require('feather-icons');
 feather.replace();
 
